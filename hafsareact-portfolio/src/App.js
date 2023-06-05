5da1eaf0d358e819e5d35c86cde404c4c5110eed
hafsaabdi880 <hafsaabdi88@outlook.com>
2023-06-05 01:57:17 +0100

    first commit

---
+++ hafsareact-portfolio/src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+import React, { useState } from "react";
+import Header from "./componets/Header";
+import Footer from "./componets/Footer/index.js";
+import About from "./componets/about";
+import Portfolio from "./componets/Portfolio";
+import Contact from "./componets/contact";
+import Resume from "./componets/Resume";
+
+function App() {
+	const [currentTab, setCurrentTab] = useState("about");
+
+	// This function checks to see which tab is selected and then generates the appropriate tab.
+	const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+			case "portfolio":
+				return <Portfolio />;
+			case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div>
+			<div className="mobile-header">
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+			<div>
+				<Footer></Footer>
+			</div>
+		</div>
+	);
+}
+
+export default App;
